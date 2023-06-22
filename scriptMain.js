cb7d8056a0ab5f9b382c9d9bfa05a7318c06e6ce
Gabriel Barbosa dos Santos <gabrielbarbosadossantos@usp.br>
2023-06-22 14:14:19 -0300

    #06.0 Carrossel

--- scriptMain.js
+++ scriptMain.js
@@ -29,3 +29,26 @@ function handleMoveToRecipes(key){
     //essa key vai ser utilizada para passar por parametro para a página de cada receita
     window.location.href = "Receita.html";
 }
+
+
+//Carrossel
+const buttonsWrapper = document.querySelector(".map");
+const slides = document.querySelector(".inner");
+
+buttonsWrapper.addEventListener("click", e => {
+  if (e.target.nodeName === "BUTTON") {
+    Array.from(buttonsWrapper.children).forEach(item =>
+      item.classList.remove("active")
+    );
+    if (e.target.classList.contains("first")) {
+      slides.style.transform = "translateX(-0%)";
+      e.target.classList.add("active");
+    } else if (e.target.classList.contains("second")) {
+      slides.style.transform = "translateX(-33.33333333333333%)";
+      e.target.classList.add("active");
+    } else if (e.target.classList.contains('third')){
+      slides.style.transform = 'translatex(-66.6666666667%)';
+      e.target.classList.add('active');
+    }
+  }
+});
